e1e5afc323343e67467ab50971c44dad29f99b72
Joonsu Yoon <bananamilk452@gmail.com>
2021-11-18 23:24:03 +0900

    fix) cant download video

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-param-reassign */
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   productionSourceMap: false,
@@ -15,12 +16,19 @@ module.exports = {
           arg[0]['process.env.FLUENTFFMPEG_COV'] = false;
           return arg;
         });
+        config.plugin('copy').use(new CopyPlugin({
+          patterns: [
+            { from: 'src/downloadVideo.js', to: 'src/downloadVideo.js' },
+            { from: 'src/downloadAudio.js', to: 'src/downloadAudio.js' },
+            { from: 'src/util.js', to: 'src/util.js' },
+          ],
+        }));
       },
       mainProcessFile: 'src/background.js',
-      mainProcessWatch: ['src/ipc.js', 'src/install.js', 'src/util.js'],
+      mainProcessWatch: ['src/ipc.js', 'src/install.js', 'src/util.js', 'src/downloadVideo.js', 'src/downloadAudio.js'],
       preload: 'src/preload.js',
       builderOptions: {
-        extraFiles: ['./bin/DO_NOT_REMOVE_THIS_DIRECTORY.txt', './src/downloadVideo.js', './src/downloadAudio.js'],
+        extraFiles: ['./bin/DO_NOT_REMOVE_THIS_DIRECTORY.txt', 'src/downloadVideo.js', 'src/downloadAudio.js', 'src/util.js'],
         appId: 'com.lemonyoutubedownloader.app',
         productName: 'LemonYoutubeDownloader',
         publish: [
